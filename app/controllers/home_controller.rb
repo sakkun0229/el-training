45ebc56142dfc3a66252b031b31d4b560940765f
tsakuma <dptr.tsakuma@gmail.com>
2018-02-15 15:26:56 +0900

    crud機能追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,47 @@
 class HomeController < ApplicationController
-  def top
+
+  def index
+    @tasks = Task.all
   end
+
+  def new
+  end
+
+  def create
+    @task = Task.new(content: params[:content])
+    if @task.save
+      flash[:notice] = "投稿を保存しました"
+      redirect_to("/")
+    else
+      render("home/new")
+    end
+  end
+
+  def show
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.content = params[:content]
+    if @task.save
+      redirect_to("/")
+      flash[:notice] = "変更を保存しました"
+    else
+      @task.content = params[:content]
+      render("task/edit")
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    @task.destroy
+    flash[:notice] = "削除しました"
+    redirect_to("/")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+
+  get '/' => "home#index"
+
+  get "new" => "home#new"
+  post "create" => "home#create"
+
+  get ":id" => "home#show"
+
+  get ":id/edit" => "home#edit"
+  post ":id/update" => "home#update"
+  post ":id/destroy" => "home#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
