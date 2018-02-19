16628383fe4f391743b62ba539d6a22d2ed54117
tsakuma <dptr.tsakuma@gmail.com>
2018-02-19 13:08:39 +0900

    passwordカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219014637) do
+ActiveRecord::Schema.define(version: 20180219040437) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180219014637) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
