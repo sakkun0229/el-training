a065c7918fe7ce8d1eafbdad8654ce446653a118
tsakuma <dptr.tsakuma@gmail.com>
2018-02-22 13:50:15 +0900

    Userモデルにadmin:integerカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222015943) do
+ActiveRecord::Schema.define(version: 20180222044708) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180222015943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "admin"
   end
 
 end
