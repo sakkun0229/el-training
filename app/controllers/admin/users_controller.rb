478671f1d611bd04ad430407584f4c993c412d63
tsakuma <dptr.tsakuma@gmail.com>
2018-02-22 12:54:00 +0900

    admin/user_controllerにnew,create追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,5 +8,15 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(name:params[:name],email:params[:email],password:params[:password])
+    if @user.save
+      flash[:notice] = "ユーザーを登録しました"
+      redirect_to("/admin/users")
+    else
+      render("/admin/users/new")
+    end
+  end
+
 
 end
