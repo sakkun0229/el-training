5f9b7a0b7138fe13d448ccc71e85f344f97b252c
tsakuma <dptr.tsakuma@gmail.com>
2018-02-26 11:46:33 +0900

    adminにタスクのedit&update機能追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,4 +3,23 @@ class Admin::PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     @user = @post.user
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.content = params[:content]
+    @post.deadline = params[:deadline]
+
+    if @post.save
+      flash[:notice] = "内容を更新しました"
+      redirect_to("/admin/users")
+    else
+      render("/admin/posts/edit")
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
     post "/users/:id/destroy" => "users#destroy"
 
     get "/:id" => 'posts#show'
+    get ":id/edit" => "posts#edit"
+    post "/:id/update" => "posts#update"
 
   end
 
